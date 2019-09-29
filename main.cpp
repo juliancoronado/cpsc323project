a82e4fe904b6fa7f0e26531f563616e7dbb3492f
SmittyWerbenjagermanjensen01 <47015670+SmittyWerbenjagermanjensen01@users.noreply.github.com>
2019-09-28 20:08:38 -0700

    Update main.cpp
    Very rough draft of source code. Reads and writes files properly.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,55 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-int main() {
-    cout << "Hello, world!" << endl;
-    return 0;
+/*struct record
+{
+	string token;
+	string lexeme;
+};
+
+
+string lexer()
+{
+
+
+
+
+
+}*/
+
+
+int main()
+{
+	//Read file character by character
+	char c;
+	ifstream infile("sample1.txt");
+	if (infile.is_open())
+	{
+		char c;
+		while (infile.get(c))
+		{
+			cout << c;
+		}
+		cout << endl;
+		infile.close();
+	}
+
+	//Display error message if file cannot be accessed
+	else cout << "Error: Unable to open file" << endl;
+
+	//Now write results to file
+	ofstream outfile;
+	outfile.open("output1.txt");
+	outfile << "Output:\n Token		Lexeme" << endl;
+	//Below line is going to replaced by loop outputting
+	//tokens and lexemes stored in struct1
+	outfile << "keyword				while" << endl;
+	outfile.close();
+
+
+	system("pause");
+	return 0;
 }
