ee9a757e0d08666531224d31838e8ac8722669ab
Julian Coronado <julian.c28@gmail.com>
2019-10-05 12:38:03 -0700

    Fixed some things

--- main.cpp
+++ main.cpp
@@ -20,36 +20,39 @@ string lexer()
 
 }*/
 
+string text;
 
 int main()
 {
 	//Read file character by character
-	char c;
-	ifstream infile("sample1.txt");
+	string temp;
+	ifstream infile("sample1");
 	if (infile.is_open())
 	{
-		char c;
-		while (infile.get(c))
+		// runs while index is not at the end of the file
+		while (!infile.eof())
 		{
-			cout << c;
+			// this will remove all the newline characters and put text into one line
+			getline(infile, temp);
+			temp += " ";
+			text += temp;
 		}
-		cout << endl;
 		infile.close();
+	} else {
+		// Displays error if file is unable to open
+		cout << "Error: Unable to open file" << endl;
 	}
 
-	//Display error message if file cannot be accessed
-	else cout << "Error: Unable to open file" << endl;
-
-	//Now write results to file
+	/* //Now write results to file
 	ofstream outfile;
 	outfile.open("output1.txt");
 	outfile << "Output:\n Token		Lexeme" << endl;
 	//Below line is going to replaced by loop outputting
 	//tokens and lexemes stored in struct1
 	outfile << "keyword				while" << endl;
-	outfile.close();
+	outfile.close(); */
 
-
-	system("pause");
+	cout << text << endl;
+	//system("pause");
 	return 0;
 }
