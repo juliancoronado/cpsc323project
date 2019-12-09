0320587b54e3050ce687c172fd72fca97caead75
SmittyWerbenjagermanjensen01 <47015670+SmittyWerbenjagermanjensen01@users.noreply.github.com>
2019-12-08 19:20:28 -0800

    Add files via upload

---
+++ itried.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <iomanip>
+#include "syntacticalAnalyzer.cpp"
+#include "main.cpp"
+
+using namespace std;
+
+string save;
+
+
+
+void A()
+{
+  if (token_state == 1)
+  {
+    save = token;
+    lexer();
+    if (token == "=")
+    {
+      lexer();
+      E();
+      //get_instr(POPM, get_address(save));
+    }
+    else cout<<"ERROR: = expected"<<endl;
+  }
+  else cout<<"ERROR: id expected"<<endl;
+};
+
+void E()
+{
+T();
+E_prime();
+};
+
+void E_prime()
+{
+  if (token == "+")
+  {
+    lexer();
+    T();
+    //gen_instr(ADD, nil);
+    E_prime();
+  }
+};
+
+void T()
+{
+  F();
+  T_prime();
+};
+
+void T_prime()
+{
+  if (token == "*")
+  {
+    lexer();
+    F();
+    //gen_instr(MUL,nil);
+    T_prime();
+  }
+};
+
+void F()
+{
+  if (token_state == 1)
+  {
+    //gen_instr(PUSHM, get_address(token));
+    lexer();
+  }
+  else
+  {
+    cout<<"ERROR: id expected"<<endl;
+  }
+};
+
+void gen_instr(op, oprnd)
+{
+  instr_table[instr_address].address = instr_address;
+  instr_table[instr_address].op = op;
+  instr_table[instr_address].oprnd = oprnd;
+  instr_address++;
+};
